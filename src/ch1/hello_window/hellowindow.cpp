46f1439a8ce2b9d6e940d3a5f139aad7aeecfbdb
Tyler Delamater <tyler.delamater@aa.com>
2022-07-22 16:59:17 -0500

    hello window excercise

--- src/ch1/hello_window/hellowindow.cpp
+++ src/ch1/hello_window/hellowindow.cpp
@@ -2,6 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main() {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -12,9 +14,30 @@ int main() {
   GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create window" << std::endl;
+    glfwTerminate();
+    return -1;
   }
+
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  
+  glViewport(0,0,800,600);
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
 
